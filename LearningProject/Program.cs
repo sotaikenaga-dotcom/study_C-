cc4c315eb1cc4bae12ab3c5440bf0200e8970865
sotaikenaga3 <sota.ikenaga3@gmail.com>
2026-04-17 15:58:51 +0900

    first commit

---
+++ LearningProject/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Console.Write("1つ目の数字を入力してね");
+        var num_a = int.Parse(Console.ReadLine()!);
+        Console.Write("2つ目の数字を入力してね");
+        var num_b = int.Parse(Console.ReadLine()!);
+        if(num_a == 1 && num_b == 1)
+        {
+            Console.WriteLine("どちらの数も1です");
+        }
+        else if(num_a == 1 || num_b ==1)
+        {
+            Console.WriteLine("どちらかの数は１です");
+        }
+        else if(!(num_a == num_b))
+        {
+            Console.WriteLine("二つの数は一致しません");
+        }
+        else
+        {
+            Console.WriteLine("1じゃないところで一致してるね");
+        }
+    }
+}
