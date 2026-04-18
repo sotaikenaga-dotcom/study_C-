0379115fe0f71c0211ed6ef212c95e31599f3fbd
IKE <sota.ikenaga3@gmail.com>
2026-04-18 19:16:01 +0900

    GuessGameの基礎

---
+++ GuessGame/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+public class Program
+{
+    public static void Main()
+    {
+        Random rnd = new Random();
+        int num_ans = rnd.Next(1, 100);
+        // 答え作成
+        Console.Write("1~100から数を予想してね");
+        var num = int.Parse(Console.ReadLine()!);
+        var count = 0
+        while(num_ans != num)
+        {
+            if(num_ans > num)
+            {
+                Console.WriteLine("もっと大きいよ");
+                Console.Write("もう一回入力してみて");
+                num = int.Parse(Console.ReadLine()!);
+            }
+            else
+            {
+                Console.WriteLine("もっと小さいよ");
+                Console.Write("もう一回入力してみて");
+                num = int.Parse(Console.ReadLine()!);
+            }
+        }
+        Console.WriteLine("正解！");
+    }
+    
+}
