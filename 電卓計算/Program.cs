68e74d8628d770fe4aa919ba33dee3d2c9df5a19
IKE <sota.ikenaga3@gmail.com>
2026-04-19 15:45:29 +0900

    電卓ver.1

---
+++ 電卓計算/Program.cs
@@ -0,0 +1,37 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Console.Write("第1項");
+        double a = double.Parse(Console.ReadLine()!);
+        Console.Write("演算子の選択(+,-,×, ÷)");
+        string Operator = Console.ReadLine()!;
+        Console.Write("第2項");
+        double b = double.Parse(Console.ReadLine()!);
+
+        if(Operator == "+")
+        {
+            Console.WriteLine("{0} + {1} = {2}",a, b, a + b);
+        }
+        else if(Operator == "-")
+        {
+            Console.WriteLine("{0} - {1} = {2}",a, b, a - b);
+        }
+        else if(Operator == "×")
+        {
+            Console.WriteLine("{0} × {1} = {2}",a, b, a * b);
+        }
+        else if(Operator == "÷")
+        {
+            if(b == 0)
+            {
+                Console.WriteLine("error");
+            }
+            else
+            {
+                Console.WriteLine("{0} ÷ {1} = {2}",a, b, a / b);
+            }
+
+        }
+    }
+}
